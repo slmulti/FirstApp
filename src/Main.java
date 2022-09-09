7b1232430ef7b156039ff2ac6e8a273a4d51d1d7
Simon Love <simon-love@hotmail.co.uk>
2022-09-09 12:34:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    /*
+    This is the first comment line
+    This is the second comment line
+    //this is a line comment
+     */
+    public static void main(String[] args){
+        //this is a standalone line comment
+        System.out.println("First line from app"); //this is a line comment
+        //System.out.println("Second line from app");
+        System.out.println(/*"Third line from app"*/"this is different text");
+    }
+}
+ 
